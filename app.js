7f401d0c6869b0f0875cfbf8c546c349c1f185fb
jkingzheng <40412945+jkingzheng@users.noreply.github.com>
2018-07-12 17:44:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/webhook', (req, res) => {
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      console.log('Sender ID: 1577199618');
+      console.log('Sender ID: 1139929316');
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
